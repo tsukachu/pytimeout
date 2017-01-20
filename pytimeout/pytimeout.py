a0823d18f3cd715df80fc82e484ef001a59bac31
tsukachu <concon.ch.1543@gmail.com>
2017-01-20 21:52:33 +0900

    Collect handler function

--- pytimeout/pytimeout.py
+++ pytimeout/pytimeout.py
@@ -4,14 +4,15 @@ from functools import wraps
 import signal
 
 
+def _handler(signum, frame):
+    raise TimeoutError
+
+
 def timeout(sec):
     def deco(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            def handler(signum, frame):
-                raise TimeoutError
-
-            signal.signal(signal.SIGALRM, handler)
+            signal.signal(signal.SIGALRM, _handler)
             signal.setitimer(signal.ITIMER_REAL, sec)
             try:
                 return func(*args, **kwargs)
@@ -22,10 +23,7 @@ def timeout(sec):
 
 
 def with_run(sec, func, *args, **kwargs):
-    def handler(signum, frame):
-        raise TimeoutError
-
-    signal.signal(signal.SIGALRM, handler)
+    signal.signal(signal.SIGALRM, _handler)
     signal.setitimer(signal.ITIMER_REAL, sec)
     try:
         return func(*args, **kwargs)
